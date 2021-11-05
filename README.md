d177dc5d3d640d92d028379aec19c81baafc041b
sabrine.ben <binabenn@outlook.fr>
2021-11-05 20:51:00 +0100

    Création de projet CRUD en php

---
+++ README.md
@@ -0,0 +1,7 @@
+#Créer un projet php hebergé sur gitlab.com
+#Ce projet sera composé de 2 vues:
+#La page d’accueil en accès public : Elle contient un formulaire d’inscription/connexion
+##Une page privée à laquelle on peut accéder avec le bon login/mot de passe
+#Le parcours utilisateur est le suivant :
+#Un visiteur arrive sur la page d’accueil, un formulaire de connexion/enregistrement lui est proposé
+#Après avoir entré les identifiants correctement l’utilisateur est redirigé vers la page privée.
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include 'db-connect.php';
+    $email = $_POST['email'];  
+    $password = $_POST['pass'];  
+    $email = stripcslashes($email);  
+    $password = stripcslashes($password);  
+    $email = mysqli_real_escape_string($con, $email);  
+    $password = mysqli_real_escape_string($con, $password);  
+    $nom= $_POST["firstname"];
+    $prenom=$_POST["lastname"];
+    $email1=$_POST["email"];
+    $password=$_POST['pass'];
+    $sql = "select * from inscription where email = '$email' and password = '$password'";  
+    $result = mysqli_query($con, $sql);  
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result); 
+    if($count == 0){  
+        $req = $con->prepare("INSERT INTO `inscription`(`firstName`, `lastName`, `email`, `password`) VALUES ('$nom', '$prenom', '$email1', '$password')");
+        $req->execute();
+        header('Location: connexion.html');
+    }  
+    else{  
+        header("Location: formulaire.html");
+        echo "<h1> Vérifier votre email ou mot de passe .</h1>";  
+    } 
+include 'formulaire.html';
+?>
