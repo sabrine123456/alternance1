192c582cdbe3a22e1cd51ab06bc4fae4a5c7802f
sabrine.ben <binabenn@outlook.fr>
2021-11-06 02:47:56 +0100

    Modifier un produit existant

--- edit.php
+++ edit.php
@@ -2,21 +2,25 @@
 include 'db-connect.php';
 include 'deconnexion.html';
 
+// afficher les produit et prix avec un id 
+$id=$_GET['id'];
+  $sql = "SELECT * FROM `produit` where id= '$id'";  
+  $req = $con->query($sql);
+  $rows = $req->fetch_all(MYSQLI_ASSOC);
+  $prod=$rows[0]["nom_produit"];
+  $price=$rows[0]["prix"];
 
-//   $sql = "SELECT * FROM `produit`";  
-//   $req = $con->query($sql);
-//   $rows = $req->fetch(MYSQLI_ASSOC);
-// $count = mysqli_num_rows($result); 
-// // $req = mysqli_query($con,$sql);
-// // $data = mysqli_fetch_array($req);
-//echo $post['prix'];
-
-    $id=$_GET['id'];
+// modifier un produit
+if(isset($_POST['produit'])){
+    $id=$_POST['id'];
     $produit= $_POST["produit"];
     $prix=$_POST["prix"];
-    $sql = "UPDATE `produit` SET `nom_produit` = '$produit', `prix` = '$prix' WHERE id = '$id' ";
-    $result = $con->prepare($sql);
-    $result->execute();
 
+    $sql1= "UPDATE `produit` SET `nom_produit`= '$produit' ,`prix`= '$prix'  WHERE id = '$id'";   
+    $result = $con->prepare($sql1);
+    $result->execute();
+    header("Location: accueil.php");
+}
+  
 
 include 'edit.phtml';
